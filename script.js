802a4e0a6c65b990958d21bbdb663140441d2141
Omar Carbajal <33193266+omarcarb@users.noreply.github.com>
2025-02-07 01:07:23 -0800

    added dropdown functiuonality

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+function OpenMenu(button){
+    let dropdownMenu = button.nextElementSibling;
+    let dropdownToggle = button.getAttribute("aria-expanded")
+
+    document.querySelectorAll(".dropdown_toggle").forEach(btn => {
+        btn.setAttribute("aria-expanded", "false");
+        btn.nextElementSibling.style.display = "none";
+    });
+
+    if(dropdownToggle == "false"){
+        button.setAttribute("aria-expanded", "true");
+        dropdownMenu.style.display ="flex"
+    }
+    else{
+        button.setAttribute("aria-expanded", "false");
+    }
+}
+document.addEventListener("click", function (event) {
+    let isClickInside = event.target.closest(".dropdown");
+
+    if (!isClickInside) {
+        document.querySelectorAll(".dropdown_toggle").forEach(btn => {
+            btn.setAttribute("aria-expanded", "false");
+            btn.nextElementSibling.style.display = "none";
+        });
+    }
+});
 document.addEventListener("DOMContentLoaded", function() {
     
 });
