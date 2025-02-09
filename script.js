c04a3488d5c4eac611a44f3954d9dd95130d0f88
Omar Carbajal <33193266+omarcarb@users.noreply.github.com>
2025-02-09 15:03:17 -0800

    added product cards and fixed json issue

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+fetch("./product_list.json")
+    .then(response => response.json())
+    .then(data => showInfo(data))
+
+function showInfo(data){
+    console.log(data)
+    data.slice(0 , 4).forEach(product => CreateProductCard(product));
+}
 
 
 function OpenMenu(button){
     let dropdownMenu = button.nextElementSibling;
     let dropdownToggle = button.getAttribute("aria-expanded")
-
     document.querySelectorAll(".dropdown_toggle").forEach(btn => {
         btn.setAttribute("aria-expanded", "false");
         btn.nextElementSibling.style.display = "none";
     });
-
     if(dropdownToggle == "false"){
         button.setAttribute("aria-expanded", "true");
         dropdownMenu.style.display ="flex"
@@ -19,7 +25,6 @@ function OpenMenu(button){
 }
 document.addEventListener("click", function (event) {
     let isClickInside = event.target.closest(".dropdown");
-
     if (!isClickInside) {
         document.querySelectorAll(".dropdown_toggle").forEach(btn => {
             btn.setAttribute("aria-expanded", "false");
@@ -27,20 +32,34 @@ document.addEventListener("click", function (event) {
         });
     }
 });
-function AddProducts(){
-    
-
-    let productItem = document.createElement('div');
-    productItem.classList.add('product_card');
-    let headingText = document.createElement('h1');
-    headingText.innerHTML = "Something something";
-
-    productItem.appendChild(headingText)
-
-    productGrid.appendChild(productItem)
-}
-
 document.addEventListener("DOMContentLoaded", function() {
     
 });
 
+function CreateProductCard(product){
+    let productGrid = document.querySelector('.section_grid')
+
+    let productCard = document.createElement('div')
+
+    productCard.classList.add('product_card')
+    let content = document.createRange().createContextualFragment(`
+        <div class=card_img>
+            <img src="https://cdn.create.web.com/images/industries/common/images/placeholder-product-image-sq.jpg" alt="" class="card_thumbnail">
+            <div class=like_button></div>
+        </div>
+        <div class=card_bottom>
+            <div class=card_text>
+                <h3>${product.name}</h3>
+                <span>$${product.price}</span>
+            </div>
+            <div class=button_container id=card>
+                <button class="secondary">Add to Cart</button>
+                <button class="primary">Buy Now</button>
+            </div>
+            
+        </div>
+    `);
+    productCard.appendChild(content);
+
+    productGrid.appendChild(productCard)
+}
